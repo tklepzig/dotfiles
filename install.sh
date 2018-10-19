127ca0967b2d397d8371232785cb72e285807cb5
Thomas Klepzig <thomas.klepzig@invision.de>
2018-10-19 18:26:23 +0200

    added echo type note

--- install.sh
+++ install.sh
@@ -20,6 +20,7 @@ isProgramInstalled()
 }
 
 accent='\033[0;33m'
+note='\033[2;33m'
 success='\033[0;92m'
 error='\033[0;91m'
 reset='\033[0m'
@@ -27,6 +28,11 @@ reset='\033[0m'
 info()
 {
     echo -e "${accent}$1${reset}"
+}
+
+note()
+{
+    echo -e "${note}$1${reset}"
 }
 
 success()
