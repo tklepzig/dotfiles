f8ffdb0003742603ec885a8662c404aa78eb6353
Thomas <Thomas@DevBook>
2018-10-20 18:47:24 +0200

    do not ask if update exists for vsc extension

--- vscode-extensions.sh
+++ vscode-extensions.sh
@@ -1,73 +1,73 @@
 #!/bin/bash
 
-code-insiders --install-extension WallabyJs.quokka-vscode
-code-insiders --install-extension formulahendry.auto-rename-tag
-code-insiders --install-extension akamud.vscode-javascript-snippet-pack
-code-insiders --install-extension christian-kohler.path-intellisense
-#code-insiders --install-extension donjayamanne.githistory
-#code-insiders --install-extension huizhou.githd
-code-insiders --install-extension eamodio.gitlens
-code-insiders --install-extension kisstkondoros.typelens
-code-insiders --install-extension michelemelluso.code-beautifier
-code-insiders --install-extension minhthai.vscode-todo-parser
-code-insiders --install-extension mrmlnc.vscode-scss
-code-insiders --install-extension pflannery.vscode-versionlens
-code-insiders --install-extension qinjia.seti-icons
-code-insiders --install-extension maptz.camelcasenavigation
-code-insiders --install-extension CoenraadS.bracket-pair-colorizer
+code-insiders --install-extension WallabyJs.quokka-vscode --force
+code-insiders --install-extension formulahendry.auto-rename-tag --force
+code-insiders --install-extension akamud.vscode-javascript-snippet-pack --force
+code-insiders --install-extension christian-kohler.path-intellisense --force
+#code-insiders --install-extension donjayamanne.githistory --force
+#code-insiders --install-extension huizhou.githd --force
+code-insiders --install-extension eamodio.gitlens --force
+code-insiders --install-extension kisstkondoros.typelens --force
+code-insiders --install-extension michelemelluso.code-beautifier --force
+code-insiders --install-extension minhthai.vscode-todo-parser --force
+code-insiders --install-extension mrmlnc.vscode-scss --force
+code-insiders --install-extension pflannery.vscode-versionlens --force
+code-insiders --install-extension qinjia.seti-icons --force
+code-insiders --install-extension maptz.camelcasenavigation --force
+code-insiders --install-extension CoenraadS.bracket-pair-colorizer --force
 
 # Testing
-#code-insiders --install-extension nwhatt.chai-snippets
-#code-insiders --install-extension spoonscen.es6-mocha-snippets
-code-insiders --install-extension Orta.vscode-jest
-code-insiders --install-extension andys8.jest-snippets
-code-insiders --install-extension thekarel.open-spec-file
-code-insiders --install-extension legfrey.javascript-test-runner
+#code-insiders --install-extension nwhatt.chai-snippets --force
+#code-insiders --install-extension spoonscen.es6-mocha-snippets --force
+code-insiders --install-extension Orta.vscode-jest --force
+code-insiders --install-extension andys8.jest-snippets --force
+code-insiders --install-extension thekarel.open-spec-file --force
+code-insiders --install-extension legfrey.javascript-test-runner --force
 
 # ES 2015, Babel
-code-insiders --install-extension dzannotti.vscode-babel-coloring
-code-insiders --install-extension dbaeumer.vscode-eslint
-# code-insiders --install-extension cmstead.jsrefactor
-code-insiders --install-extension jeremyrajan.react-component
-code-insiders --install-extension wangtao0101.vscode-js-import
+code-insiders --install-extension dzannotti.vscode-babel-coloring --force
+code-insiders --install-extension dbaeumer.vscode-eslint --force
+# code-insiders --install-extension cmstead.jsrefactor --force
+code-insiders --install-extension jeremyrajan.react-component --force
+code-insiders --install-extension wangtao0101.vscode-js-import --force
 
 # Prettier
-code-insiders --install-extension esbenp.prettier-vscode
+code-insiders --install-extension esbenp.prettier-vscode --force
 
 # Flow
-code-insiders --install-extension flowtype.flow-for-vscode
-code-insiders --install-extension lsadam0.ReactFlowSnippets
+code-insiders --install-extension flowtype.flow-for-vscode --force
+code-insiders --install-extension lsadam0.ReactFlowSnippets --force
 
 # Styled Components
-code-insiders --install-extension jpoissonnier.vscode-styled-components
+code-insiders --install-extension jpoissonnier.vscode-styled-components --force
 
 # Typescript
-#code-insiders --install-extension rbbit.typescript-hero # Ein Typescript Import Assistant, der alle Imports einer Datei auf einmal auflösen kann
-#code-insiders --install-extension pmneo.tsimporter # Ebenfalls ein Typescript Import Assistant, aber der unterstützt die tsconfig.json
-code-insiders --install-extension eg2.tslint
-code-insiders --install-extension christianoetterli.refactorix
-code-insiders --install-extension infeng.vscode-react-typescript
+#code-insiders --install-extension rbbit.typescript-hero --force # Ein Typescript Import Assistant, der alle Imports einer Datei auf einmal auflösen kann
+#code-insiders --install-extension pmneo.tsimporter --force # Ebenfalls ein Typescript Import Assistant, aber der unterstützt die tsconfig.json
+code-insiders --install-extension eg2.tslint --force
+code-insiders --install-extension christianoetterli.refactorix --force
+code-insiders --install-extension infeng.vscode-react-typescript --force
 
 # Angular
-#code-insiders --install-extension johnpapa.Angular2
-#code-insiders --install-extension Angular.ng-template
+#code-insiders --install-extension johnpapa.Angular2 --force
+#code-insiders --install-extension Angular.ng-template --force
 
 # C#
-#code-insiders --install-extension ms-vscode.csharp
-#code-insiders --install-extension cake-build.cake-vscode
+#code-insiders --install-extension ms-vscode.csharp --force
+#code-insiders --install-extension cake-build.cake-vscode --force
 
 # Docker
-code-insiders --install-extension PeterJausovec.vscode-docker
+code-insiders --install-extension PeterJausovec.vscode-docker --force
 
 # Debugging
-code-insiders --install-extension msjsdiag.debugger-for-chrome
+code-insiders --install-extension msjsdiag.debugger-for-chrome --force
 
 # npm
-code-insiders --install-extension eg2.vscode-npm-script
-code-insiders --install-extension jasonnutter.search-node-modules
+code-insiders --install-extension eg2.vscode-npm-script --force
+code-insiders --install-extension jasonnutter.search-node-modules --force
 
 # systemd unit files
-code-insiders --install-extension coolbear.systemd-unit-file
+code-insiders --install-extension coolbear.systemd-unit-file --force
 
 # .vim files
-code-insiders --install-extension dunstontc.viml
+code-insiders --install-extension dunstontc.viml --force
