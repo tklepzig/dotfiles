5521e9024961598de3b6897560ec13020764be4e
Thomas <Thomas@DevBook>
2019-02-08 21:49:48 +0100

    Add alias for docker

--- bashrc.sh
+++ bashrc.sh
@@ -94,6 +94,7 @@ alias gk='LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 gitk --all &'
 alias gg='LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 git gui &'
 alias gsa="$dotfilesDir/git-status-all.sh"
 alias c='code-insiders .'
+alias d='docker'
 
 export GIT_PS1_SHOWDIRTYSTATE=1
 export PS1='\u@\h:\[\033[0;33m\]\w\[\033[01;32m\]`__git_ps1`\[\033[00m\]\n\$ '
