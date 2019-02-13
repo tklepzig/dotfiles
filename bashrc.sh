aac62b408b05d4dd1cdd9d755211396b047d5ea9
Thomas Klepzig <thomas.klepzig@invision.de>
2019-02-13 11:54:13 +0100

    Add bash completion for docker aliases

--- bashrc.sh
+++ bashrc.sh
@@ -94,8 +94,11 @@ alias gk='LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 gitk --all &'
 alias gg='LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 git gui &'
 alias gsa="$dotfilesDir/git-status-all.sh"
 alias c='code-insiders .'
+
 alias d='docker'
+complete -F _docker d
 alias dc='docker-compose'
+complete -F _docker-compose dc
 
 export GIT_PS1_SHOWDIRTYSTATE=1
 export PS1='\u@\h:\[\033[0;33m\]\w\[\033[01;32m\]`__git_ps1`\[\033[00m\]\n\$ '
