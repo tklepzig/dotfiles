bf08ccd874fa6e082cf7afad8deab67054f97c9a
Thomas <thomas@DevPad>
2020-05-07 22:04:48 +0200

    Extract aliases

---
+++ alias.sh
@@ -0,0 +1,50 @@
+alias mkcd='function __mkcd() { mkdir "$1"; cd "$1"; unset -f __mkcd; }; __mkcd'
+
+if isOS darwin
+then
+    alias ls='ls -FG'
+else
+    alias ls='ls -F --color=auto'
+fi
+
+alias la='ls -A'
+alias ll='ls -lh'
+alias lla='ls -Ahl'
+alias rmr='rm -rf'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+alias t='tmux'
+
+alias hgrep="$dotfilesDir/hgrep.sh"
+alias b="$dotfilesDir/bookmark.sh"
+alias dotfiles-update='curl -Ls https://raw.githubusercontent.com/tklepzig/dotfiles/master/install.sh | bash -s -- -u'
+alias dotfiles-update-include-vsc='curl -Ls https://raw.githubusercontent.com/tklepzig/dotfiles/master/install.sh | bash -s -- -u --include-vsc'
+alias dotfiles-tabula-rasa="$dotfilesDir/tabula-rasa.sh"
+if isOS linux
+then
+    alias n='nautilus .'
+fi
+
+if isOS darwin
+then
+    alias o='open .'
+fi
+
+alias git='LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 git'
+alias g='git'
+alias gk='LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 gitk --all &'
+alias gg='LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 git gui &'
+alias gsa="$dotfilesDir/git-status-all.sh"
+alias c='code-insiders .'
+alias d='docker'
+alias dc='docker-compose'
+alias dce='docker-compose exec'
+alias dcl='docker-compose logs -f'
+alias dp='docker system prune -f && docker rmi -f $(docker images -q)'
+alias ccp='xclip -selection clipboard'
+alias v='vi .'
+alias r='ranger'
+alias q='exit'
+alias ..='cd ..'
+alias ly="lynx -cfg $dotfilesDir/lynx.cfg"
