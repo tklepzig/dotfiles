547eddfdfa34f4f6d6b9f25f03d601007102d7d0
Thomas <thomas@DevPad>
2020-06-27 16:36:56 +0200

    test

--- test.sh
+++ test.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
 profiles="basic,extended"
-hasProfile() {
-  echo $profiles
-  for i in ${profiles/,/ }
-  do
-    echo $i
-    echo $1
-    if [ "$i" = "$1" ]
-    then
-      return 0
-    fi
-  done
-  return 1
-}
+IFS=',' read -r -a array <<< "$profiles"
+for element in "${array[@]}"
+do
+    echo "$element"
+done
 
-if hasProfile basic
-then
-  echo "has basic"
-fi
+#hasProfile() {
+  #echo $profiles
+  #for i in ${profiles/,/ }
+  #do
+    #echo $i
+    #echo $1
+    #if [ "$i" = "$1" ]
+    #then
+      #return 0
+    #fi
+  #done
+  #return 1
+#}
 
-if hasProfile extended
-then
-  echo "has extended"
-fi
+#if hasProfile basic
+#then
+  #echo "has basic"
+#fi
+
+#if hasProfile extended
+#then
+  #echo "has extended"
+#fi
