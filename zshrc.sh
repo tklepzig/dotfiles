81a07ae8503455ebc2a37ba1664f74ee3fd9459c
Thomas Klepzig <thomas.klepzig@invision.de>
2020-07-08 11:42:25 +0200

    Add elapsed time to prompt

--- zshrc.sh
+++ zshrc.sh
@@ -11,6 +11,7 @@ isOS()
 
     return 1;
 }
+NEWLINE=$'\n'
 
 source $dotfilesDir/alias.sh
 
@@ -41,64 +42,87 @@ bindkey -e
 autoload -Uz vcs_info
 setopt prompt_subst
 
-precmd() {
-    vcs_info
-    if [[ -n ${vcs_info_msg_0_} ]]; then
-        upstream=$(git rev-parse --abbrev-ref --symbolic-full-name @{u} 2> /dev/null)
-
-        if [[ -z $upstream ]] then
-            upstream_prompt="#";
-        else
-            upstream_info=$(git rev-list --left-right --count $upstream...HEAD 2> /dev/null)
-            case "$upstream_info" in
-                "") # no upstream
-                    upstream_prompt="" ;;
-                "0	0") # equal to upstream
-                    upstream_prompt="=" ;;
-                "0	"*) # ahead of upstream
-                    upstream_prompt=">" ;;
-                *"	0") # behind upstream
-                    upstream_prompt="<" ;;
-                *)	    # diverged from upstream
-                    upstream_prompt="<>" ;;
-            esac
-        fi
-
-        # vcs_info found something (the documentation got that backwards
-        # STATUS line taken from https://github.com/robbyrussell/oh-my-zsh/blob/master/lib/git.zsh
-        STATUS=$(command git status --porcelain 2> /dev/null | tail -n1)
-        if [[ -n $STATUS ]]; then
-            RPROMPT='%{$fg_bold[red]%}${vcs_info_msg_0_} %{$fg_bold[cyan]%}$upstream_prompt%{$reset_color%}'
-        else
-            RPROMPT='%{$fg_bold[green]%}${vcs_info_msg_0_} %{$fg_bold[cyan]%}$upstream_prompt%{$reset_color%}'
-        fi
-    else
-        # nothing from vcs_info
-
-        # sample for github infos
-        # token=$(cat .gh-token)
-        # response=$(curl -s "https://api.github.com/search/issues?q=repo:user/repo+type:issue+state:open&access_token=$token" 2>/dev/null)
-        # issueCount=$(echo $response | sed -En "s/^.*\"total_count\": ([0-9]+),.*$/\1/p")
-        # response=$(curl -s "https://api.github.com/search/issues?q=repo:user/repo+type:pr+state:open&access_token=$token" 2>/dev/null)
-        # prCount=$(echo $response | sed -En "s/^.*\"total_count\": ([0-9]+),.*$/\1/p")
-
-        battery=$(source $dotfilesDir/battery.sh)
-        if [[ -z $battery ]]
-        then
-          RPROMPT='%T'
-        else
-          batteryColor=""
-          if [[ $battery -lt 16 ]]
-          then
-            batteryColor="%{$fg_bold[red]%}"
-          fi
-          RPROMPT='%T | $batteryColor$battery%%%{$reset_color%}'
-        fi
-    fi
+preexec() {
+  if isOS darwin
+  then
+    timer=$(($(print -P %D{%s%6.})/1000))
+  else
+    timer=$(($(date +%s%0N)/1000000))
+  fi
 }
 
-NEWLINE=$'\n'
-PROMPT="%n@%m:%{$fg[yellow]%}%~%{$reset_color%}${NEWLINE}\$ "
+precmd() {
+  if [ $timer ]; then
+    if isOS darwin
+    then
+      now=$(($(print -P %D{%s%6.})/1000))
+    else
+      now=$(($(date +%s%0N)/1000000))
+    fi
+
+    elapsed=$(($now-$timer))
+    elapsedPrompt="%F{cyan}Elapsed Time: ${elapsed}ms%{$reset_color%}${NEWLINE}"
+    unset timer
+  fi
+
+  vcs_info
+  if [[ -n ${vcs_info_msg_0_} ]]; then
+    upstream=$(git rev-parse --abbrev-ref --symbolic-full-name @{u} 2> /dev/null)
+
+    if [[ -z $upstream ]] then
+      upstream_prompt="#";
+    else
+      upstream_info=$(git rev-list --left-right --count $upstream...HEAD 2> /dev/null)
+      case "$upstream_info" in
+        "") # no upstream
+          upstream_prompt="" ;;
+        "0	0") # equal to upstream
+          upstream_prompt="=" ;;
+        "0	"*) # ahead of upstream
+          upstream_prompt=">" ;;
+        *"	0") # behind upstream
+          upstream_prompt="<" ;;
+        *)	    # diverged from upstream
+          upstream_prompt="<>" ;;
+      esac
+    fi
+
+    # vcs_info found something (the documentation got that backwards
+    # STATUS line taken from https://github.com/robbyrussell/oh-my-zsh/blob/master/lib/git.zsh
+    STATUS=$(command git status --porcelain 2> /dev/null | tail -n1)
+    if [[ -n $STATUS ]]; then
+      RPROMPT='%{$fg_bold[red]%}${vcs_info_msg_0_} %{$fg_bold[cyan]%}$upstream_prompt%{$reset_color%}'
+    else
+      RPROMPT='%{$fg_bold[green]%}${vcs_info_msg_0_} %{$fg_bold[cyan]%}$upstream_prompt%{$reset_color%}'
+    fi
+  else
+    # nothing from vcs_info
+
+    # sample for github infos
+    # token=$(cat .gh-token)
+    # response=$(curl -s "https://api.github.com/search/issues?q=repo:user/repo+type:issue+state:open&access_token=$token" 2>/dev/null)
+    # issueCount=$(echo $response | sed -En "s/^.*\"total_count\": ([0-9]+),.*$/\1/p")
+    # response=$(curl -s "https://api.github.com/search/issues?q=repo:user/repo+type:pr+state:open&access_token=$token" 2>/dev/null)
+    # prCount=$(echo $response | sed -En "s/^.*\"total_count\": ([0-9]+),.*$/\1/p")
+
+    battery=$(source $dotfilesDir/battery.sh)
+    if [[ -z $battery ]]
+    then
+      RPROMPT='%T'
+    else
+      batteryColor=""
+      if [[ $battery -lt 16 ]]
+      then
+        batteryColor="%{$fg_bold[red]%}"
+      fi
+
+      RPROMPT='%T | $batteryColor$battery%%%{$reset_color%}'
+    fi
+  fi
+}
+
+
+PROMPT='$elapsedPrompt%n@%m:%{$fg[yellow]%}%~%{$reset_color%}${NEWLINE}\$ '
 zstyle ':vcs_info:git:*' formats "%b"
 zstyle ':vcs_info:git:*' actionformats "%b %{$reset_color%}%{$fg_bold[blue]%}(%a)%{$reset_color%}"
 zstyle ':vcs_info:*' enable git
