c647c504036f5b8578682ca578ab2f03d7855552
Thomas Klepzig <thomas.klepzig@invision.de>
2021-02-25 17:02:15 +0100

    Fix battery visualization

--- tmux/battery.osx.sh
+++ tmux/battery.osx.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-value=$(pmset -g batt | egrep "([0-9]+)\%.*" -o --colour=auto | cut -f1 -d';')
+value=$(pmset -g batt | egrep "([0-9]+)\%.*" -o --colour=auto | cut -f1 -d'%')
 
 if [[ $value -lt 16 ]]
 then
-    echo "#[fg=colour196]$value"
+    echo "#[fg=colour196]$value%"
 else
-    echo "$value"
+    echo "$value%"
 fi
