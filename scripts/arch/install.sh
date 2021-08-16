77679c0d1d3a1e48292446381fae4554890304db
Thomas Klepzig <thomas.klepzig@ivx.com>
2021-08-16 16:10:06 +0200

    Remove arch scripts in favour of using docs/arch-linux.md

--- scripts/arch/install.sh
+++
@@ -1,2 +0,0 @@
-pacstrap /mnt base base-devel linux linux-firmware vim iwd terminus-font man-db man-pages texinfo networkmanager
-genfstab -U /mnt >> /mnt/etc/fstab
--- scripts/arch/start.sh
+++
@@ -1,5 +0,0 @@
-#loadkeys de-latin1
-#setfont ter-132b
-#iwctl station {wlan interface --> use station list to get it} connect SSID
-timedatectl set-ntp true
-timedatectl set-timezone Europe/Berlin
