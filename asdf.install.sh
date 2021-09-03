245dd348e56e94f5e1509c9c07eee992271c51ee
Thomas <thomas@DevPad>
2021-09-03 22:05:28 +0200

    Add asdf install script

---
+++ asdf.install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env zsh
+
+if [ -d "$HOME/.asdf" ]
+then
+  echo "Warning: asdf already set up, exiting."
+  exit 1
+fi
+
+git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf
+cd $HOME/.asdf
+git checkout "$(git describe --abbrev=0 --tags)"
+cd -
+
+for plugin in nodejs python
+do
+  asdf plugin add $plugin
+  asdf install $plugin latest
+  asdf global $plugin latest
+done
