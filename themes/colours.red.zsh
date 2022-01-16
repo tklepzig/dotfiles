86304fe8fbf7b9483ee175862439603f7b9183c5
Thomas <Thomas@DevPad>
2022-01-16 21:43:16 +0100

    wip

---
+++ themes/colours.red.zsh
@@ -0,0 +1,31 @@
+primaryFg=7
+primaryBg=52
+primaryText=172
+secondaryFg=7
+secondaryBg=52
+secondaryText=179
+accentFg=0
+accentBg=124
+accentText=32
+
+inactivePaneBorder=244
+inactivePaneBackground=236
+
+infoFg=7
+infoBg=238
+warningFg=0
+warningBg=220
+criticalFg=0
+criticalBg=124
+
+# primary 166
+
+# light todo
+#primaryBg=24
+#primaryLightBg=32
+##primaryLighterBg=29
+#primaryLighterBg=32
+#primaryFg=15
+#accentBg=172
+#accentFg=232
+
