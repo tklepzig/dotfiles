8484fe0c488845010ada30aaeb9d525b1aff0185
Thomas Klepzig <thomas.klepzig@ivx.com>
2022-01-26 15:26:28 +0100

    wip

--- themes/colours.shield.zsh
+++ themes/colours.shield.zsh
@@ -1,12 +1,49 @@
-primaryFg=7
-primaryBg=239
-primaryText=6
-secondaryFg=7
-secondaryBg=19
-secondaryText=179
+# Idea A
+#primaryFg=0
+#primaryBg=202
+#primaryText=202
+#secondaryFg=0
+#secondaryBg=39
+#secondaryText=39
+#accentFg=0
+#accentBg=50
+#accentText=50
+
+
+# Idea B
+#primaryFg=0
+#primaryBg=202
+#primaryText=50
+#secondaryFg=0
+#secondaryBg=166
+#secondaryText=179
+#accentFg=0
+#accentBg=50
+#accentText=50
+
+
+# Idea C
+#primaryFg=254
+#primaryBg=234
+#primaryText=50
+#secondaryFg=7
+#secondaryBg=19
+#secondaryText=179
+#accentFg=0
+#accentBg=50
+#accentText=50
+
+# --------
+
+primaryFg=0
+primaryBg=202
+primaryText=202
+secondaryFg=0
+secondaryBg=39
+secondaryText=39
 accentFg=0
-accentBg=6
-accentText=32
+accentBg=50
+accentText=50
 
 inactivePaneBorder=244
 inactivePaneBackground=236
@@ -17,4 +54,5 @@ warningFg=0
 warningBg=220
 criticalFg=0
 criticalBg=124
+
 
