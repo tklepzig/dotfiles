8947e830e89511f2484d4b4fbbc2979683e68dc2
Thomas Klepzig <thomas.klepzig@ivx.com>
2022-03-08 11:17:24 +0100

    profile stuff

---
+++ zsh/ruby/alias.zsh
@@ -0,0 +1,9 @@
+alias b="bundle"
+alias ba="bundle add"
+alias be="bundle exec"
+alias ber="bundle exec rake"
+alias beru="ENV=test bundle exec rake unit"
+alias beri="ENV=test bundle exec rake integration"
+alias berui="ENV=test bundle exec rake unit integration"
+alias mr="$HOME/development/maintenance-helper/maintain-ruby.rb"
+
---
+++ zsh/ruby/zshrc.zsh
@@ -0,0 +1,4 @@
+source $dotfilesDir/zsh/ruby/alias.zsh
+
+export ASDF_RUBY_BUILD_VERSION=master
+
