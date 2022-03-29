8e5e02921a7da91cb42208c83380fc3140628864
Thomas Klepzig <thomas.klepzig@ivx.com>
2022-03-29 10:45:07 +0200

    Install fzf via git, not brew

--- brew.install.zsh
+++ brew.install.zsh
@@ -8,8 +8,11 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 brew install git
 brew install --cask kitty google-chrome
 
-brew install wget wemux websocat btop lynx ddgr exa fzf pqiv ranger the_silver_searcher tig tmux bat pam-reattach
+brew install wget wemux websocat btop lynx ddgr exa pqiv ranger the_silver_searcher tig tmux bat pam-reattach
 brew install --cask gimp insomnia licecap raycast vnc-viewer ultimaker-cura seafile-client audacity vlc iterm2
 
 brew install awscli
 brew install --cask docker browserstacklocal slack discord tunnelblick gpg-suite visual-studio-code
+
+git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
+$HOME/.fzf/install
