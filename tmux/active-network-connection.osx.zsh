bd03692095fae6fc095edd3f236146bedadc90e2
Thomas <Thomas@DevPad>
2022-04-18 15:49:55 +0200

    Use status-format to adjust statusline with many open windows

--- tmux/active-network-connection.osx.zsh
+++ tmux/active-network-connection.osx.zsh
@@ -4,16 +4,16 @@ routeResult=$(route get google.de 2>&1)
 
 if [[ "$routeResult" == *"bad address"* ]]
 then
-    echo "Offline"
+    echo " Offline "
 else
     interface=$(echo "$routeResult" | grep interface | awk '{print $2}')
 
     if [[ "$interface" == *"utun"* ]]
     then
-        echo "VPN"
+        echo " VPN "
     else
         device=$(networksetup -listnetworkserviceorder | grep $interface | sed -E -n 's/.*: (.*),.*/\1/p')
-        echo "$device"
+        echo " $device "
     fi
 fi
 
