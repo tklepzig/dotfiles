5ea9fe76838ba712e225d87dbad6deb0007dc0ad
Thomas <Thomas@DevPad>
2022-04-23 11:41:52 +0200

    Fix too much space on linux

--- tmux/active-network-connection.osx.zsh
+++ tmux/active-network-connection.osx.zsh
@@ -4,16 +4,16 @@ routeResult=$(route get google.de 2>&1)
 
 if [[ "$routeResult" == *"bad address"* ]]
 then
-    echo " Offline "
+    echo "#[default] #[fg=$primaryFg,bg=$primaryBg] Offline "
 else
     interface=$(echo "$routeResult" | grep interface | awk '{print $2}')
 
     if [[ "$interface" == *"utun"* ]]
     then
-        echo " VPN "
+        echo "#[default] #[fg=$primaryFg,bg=$primaryBg] VPN "
     else
         device=$(networksetup -listnetworkserviceorder | grep $interface | sed -E -n 's/.*: (.*),.*/\1/p')
-        echo " $device "
+        echo "#[default] #[fg=$primaryFg,bg=$primaryBg] $device "
     fi
 fi
 
