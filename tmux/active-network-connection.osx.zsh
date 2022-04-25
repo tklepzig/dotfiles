23d63d981c18f3caf6aa0d08301a3b2386dfeee1
Thomas Klepzig <thomas.klepzig@ivx.com>
2022-04-25 13:18:23 +0200

    Fix network display

--- tmux/active-network-connection.osx.zsh
+++ tmux/active-network-connection.osx.zsh
@@ -4,16 +4,16 @@ routeResult=$(route get google.de 2>&1)
 
 if [[ "$routeResult" == *"bad address"* ]]
 then
-    echo "#[default] #[fg=$primaryFg,bg=$primaryBg] Offline "
+    echo "#[default] #[fg=$secondaryFg,bg=$secondaryBg] Offline #[default] "
 else
     interface=$(echo "$routeResult" | grep interface | awk '{print $2}')
 
     if [[ "$interface" == *"utun"* ]]
     then
-        echo "#[default] #[fg=$primaryFg,bg=$primaryBg] VPN "
+        echo "#[default] #[fg=$secondaryFg,bg=$secondaryBg] VPN #[default] "
     else
         device=$(networksetup -listnetworkserviceorder | grep $interface | sed -E -n 's/.*: (.*),.*/\1/p')
-        echo "#[default] #[fg=$primaryFg,bg=$primaryBg] $device "
+        echo "#[default] #[fg=$secondaryFg,bg=$secondaryBg] $device #[default] "
     fi
 fi
 
