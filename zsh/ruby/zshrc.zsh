f0ce7768880da548fea4c18d24b41bd53a1a05a6
Thomas Klepzig <thomas.klepzig@ivx.com>
2022-08-25 11:01:53 +0200

    Add explanation

--- zsh/ruby/zshrc.zsh
+++ zsh/ruby/zshrc.zsh
@@ -1,5 +1,6 @@
 source $dotfilesDir/zsh/ruby/alias.zsh
 
 export ASDF_RUBY_BUILD_VERSION=master
+# Don't do that, some gems don't care about frozen and will crash with this set as default
 #export RUBYOPT=--enable-frozen-string-literal
 
