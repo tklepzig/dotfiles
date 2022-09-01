1c9651c622f512143e906f7917f464ecc2f15de7
Thomas Klepzig <thomas.klepzig@ivx.com>
2022-09-01 13:28:25 +0200

    Update ruby spec template

--- vim/ruby/skeletons/spec.rb
+++ vim/ruby/skeletons/spec.rb
@@ -1,2 +1,24 @@
-#Blubb
-#sth with rspec
+# frozen_string_literal: true
+
+require 'SUT'
+
+RSpec.describe SUT do
+  subject(:sut) { described_class.new }
+
+  let(:anything) { 42 }
+
+  describe '#instance_method' do
+    it 'does sth' do
+    end
+  end
+
+  describe '.class_methods' do
+    it 'does sth' do
+    end
+
+    context 'with sth' do
+      it 'does sth else' do
+      end
+    end
+  end
+end
