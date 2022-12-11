d3bc9f9f402e069fcd8061f3c3adf2e3e508d0c3
Thomas <Thomas@DevPad>
2022-12-11 12:18:31 +0100

    Make git-fetch-all a bit more verbose

--- git/git-fetch-all.sh
+++ git/git-fetch-all.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
 
 maxdepth=${1-1}
-find . -maxdepth $maxdepth -mindepth 0 -type d -exec sh -c "test -d \"{}/.git\" && cd \"{}\" && git fetch" \;
+find . -maxdepth $maxdepth -mindepth 0 -type d -exec sh -c "test -d \"{}/.git\" && echo \"Fetching {}\" && cd \"{}\" && git fetch" \;
