0a68d681e428043e559281c3749aa623c1f7cf2e
Thomas <Thomas@DevPad>
2022-12-11 12:03:02 +0100

    Add git-fetch-all

--- zsh/basic/alias.zsh
+++ zsh/basic/alias.zsh
@@ -44,6 +44,7 @@ alias g='git'
 alias gk='LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 gitk --all &'
 alias gg='LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 git gui &'
 alias gsa="$dotfilesDir/git/git-status-all.sh"
+alias gfa="$dotfilesDir/git/git-fetch-all.sh"
 alias c='clear'
 alias d='docker'
 alias dc='docker compose'
