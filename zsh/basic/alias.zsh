788c5c2797fb704762c38c42a941fe8ac1b73fd0
Thomas Klepzig <thomas.klepzig@ivx.com>
2023-01-30 14:51:18 +0100

    Messing with quotes

--- zsh/basic/alias.zsh
+++ zsh/basic/alias.zsh
@@ -28,7 +28,7 @@ alias egrep='egrep --color=auto'
 alias t='tmux new-session -n ""'
 alias ta='tmux a'
 
-alias dotfiles-update="/usr/bin/env ruby -e '$(curl -Ls https://raw.githubusercontent.com/$dotfilesRepo/master/setup.rb)'"
+alias dotfiles-update='/usr/bin/env ruby -e "$(curl -Ls https://raw.githubusercontent.com/$dotfilesRepo/master/setup.rb)"'
 alias dfu='dotfiles-update'
 alias dotfiles-update-legacy='/usr/bin/env zsh -c "$(curl -Ls https://raw.githubusercontent.com/tklepzig/dotfiles/master/install.sh)"'
 alias dotfiles-tabula-rasa="$dotfilesDir/tabula-rasa.sh"
