e57cd0ffa166a4979b9645dae4d22361eed99a7b
Thomas Klepzig <thomas.klepzig@ivx.com>
2023-02-17 09:29:14 +0100

    Add ruby configure option for troubleshooting

--- zsh/ruby/zshrc.zsh
+++ zsh/ruby/zshrc.zsh
@@ -4,3 +4,5 @@ export ASDF_RUBY_BUILD_VERSION=master
 # Don't do that, some gems don't care about frozen and will crash with this set as default
 #export RUBYOPT=--enable-frozen-string-literal
 
+# In case instaling ruby via asdf fails due to missing header files, try the following
+#export RUBY_CONFIGURE_OPTS="--with-zlib-dir=$(brew --prefix zlib) --with-openssl-dir=$(brew --prefix openssl@3) --with-readline-dir=$(brew --prefix readline) --with-libyaml-dir=$(brew --prefix libyaml)"
