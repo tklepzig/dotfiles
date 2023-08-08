be35397fc9721de1ff38779efe445b7506399c5a
Thomas Klepzig <thomas.klepzig@ivx.com>
2023-08-08 10:51:40 +0200

    Fix npm i

--- setup.rb
+++ setup.rb
@@ -190,9 +190,7 @@ def install
   add_link_with_override "#{HOME}/.plugins.vim", "#{HOME}/.vimrc"
 
   Logger.log 'Initializing toolbox...', newline: false
-  `cd #{DF_PATH}/toolbox/tools/vicy`
-  `npm i && npm run build`
-  `cd -`
+  `cd #{DF_PATH}/toolbox/tools/vicy && npm i && npm run build && cd -`
   add_link_with_override "#{DF_PATH}/toolbox/init.zsh", "#{HOME}/.zshrc"
   Logger.log ' Done.'.success
 
