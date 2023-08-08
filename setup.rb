3411f8336373c107f684112561a075fb484a1778
Thomas Klepzig <thomas.klepzig@ivx.com>
2023-08-08 11:30:40 +0200

    Fix spacing

--- setup.rb
+++ setup.rb
@@ -192,7 +192,7 @@ def install
   Logger.log 'Initializing toolbox...'
   `cd #{DF_PATH}/toolbox/tools/vicy && npm i > /dev/null 2>&1 && npm run build > /dev/null 2>&1 && cd -`
   add_link_with_override "#{DF_PATH}/toolbox/init.zsh", "#{HOME}/.zshrc"
-  Logger.log ' Done.'.success
+  Logger.log 'Done.'.success
 
   install_profiles
 
