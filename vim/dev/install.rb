43030b9691419d0c62eed2c8b6eec071465d2a37
Thomas Klepzig <thomas.klepzig@ivx.com>
2023-11-14 10:21:56 +0100

    Correctly link coc settings for nvim

--- vim/dev/install.rb
+++ vim/dev/install.rb
@@ -7,3 +7,8 @@ check_optional_installation 'bat'
 
 `mkdir -p #{HOME}/.vim`
 `ln -sf #{DF_PATH}/vim/dev/coc-settings.json #{HOME}/.vim/coc-settings.json`
+
+if ENV['DOTFILES_NVIM']
+  `mkdir -p #{HOME}/.config/nvim`
+  `ln -sf #{DF_PATH}/vim/dev/coc-settings.json #{HOME}/.config/nvim/coc-settings.json`
+end
