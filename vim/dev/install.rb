82cd1135ea33a861b969912757e652f351540ad6
Thomas Klepzig <tklepzig@users.noreply.github.com>
2024-07-21 19:15:10 +0200

    Use rg instead of ag for file searching

--- vim/dev/install.rb
+++ vim/dev/install.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-check_optional_installation 'ag', 'the_silver_searcher'
+check_optional_installation 'rg', 'ripgrep'
 check_optional_installation 'ranger'
 check_optional_installation 'bat'
 
