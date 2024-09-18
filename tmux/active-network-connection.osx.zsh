c91e004b269e5ff02828e8d0af9a07c960f7eea6
Thomas Klepzig <tklepzig@users.noreply.github.com>
2024-09-18 15:56:11 +0200

    Remove SSID since airport has been depreacted in OSX Sequoia

--- tmux/active-network-connection.osx.zsh
+++ tmux/active-network-connection.osx.zsh
@@ -13,8 +13,7 @@ else
         echo "#[fg=$secondaryFg,bg=$secondaryBg] VPN #[default] "
     else
         device=$(networksetup -listnetworkserviceorder | grep $interface | sed -E -n 's/.*: (.*),.*/\1/p')
-        ssid=$(/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I  | awk -F' SSID: '  '/ SSID: / {print $2}')
-        echo "#[fg=$secondaryFg,bg=$secondaryBg] $device ($ssid) #[default] "
+        echo "#[fg=$secondaryFg,bg=$secondaryBg] $device #[default] "
     fi
 fi
 
