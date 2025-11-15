c46c6ba926b49cc48ab238392bb927ca028cb773
Thomas Klepzig <tklepzig@users.noreply.github.com>
2025-11-15 13:31:43 +0100

    theming

--- themes/colours.red.zsh
+++ themes/colours.red.zsh
@@ -1,9 +1,16 @@
 primaryFg=7
 primaryBg=52
 primaryText=124
+# alternative version
+#primaryBg=88
+#primaryText=88
 secondaryFg=7
 secondaryBg=235
 secondaryText=245
+# alternative version
+#secondaryFg=0
+#secondaryBg=94
+#secondaryText=94
 accentFg=0
 accentBg=160
 accentText=196
